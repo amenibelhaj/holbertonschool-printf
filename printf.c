f1b91f653bc73ad78ff30ac9378d20cc0ef52015
nouranbedoui <nuran.bedoui.000@gmail.com>
2024-11-29 10:05:21 +0100

    sparating the two files

--- printf.c
+++ printf.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <unistd.h>
-#include <stdarg.h>
 
 /**
  * _printf - Produces output based on a format string.
@@ -51,49 +49,5 @@ count += write(1, ptr, 1);
 ptr++;
 }
 va_end(args);
-return (count);
-}
-
-
-/**
- * print_integer - Helper function to print an integer.
- * @num: The integer to print.
- * Return: The number of characters printed.
- */
-int print_integer(int num)
-{
-int count = 0;
-int temp = num;
-int digits = 0;
-int i;
-int j;
-int divisor;
-char digit;
-if (num < 0)
-{
-count += write(1, "-", 1);
-num = -num;
-}
-temp = num;
-while (temp != 0)
-{
-digits++;
-temp /= 10;
-}
-if (num == 0)
-{
-count += write(1, "0", 1);
-return count;
-}
-for (i = digits - 1; i >= 0; i--)
-{
-divisor = 1;
-for (j = 1; j < digits; j++)
-divisor *= 10;
-digit = (num / divisor) + '0';
-count += write(1, &digit, 1);
-num %= divisor;
-digits--;
-}
 return (count);
 }
