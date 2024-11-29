0fb6fcb02b4d37435f7e6d87c0e0e5e6a653a7bc
amenibelhaj <blameni073@gmail.com>
2024-11-29 13:23:44 +0200

    fixing n1000

--- _printf.c
+++ _printf.c
@@ -5,6 +5,7 @@
  * @format: The format string containing characters and format specifiers.
  * Return: The total number of characters printed (excluding the null byte).
  */
+
 int _printf(const char *format, ...)
 {
 va_list args;
@@ -15,8 +16,6 @@ return (-1);
 va_start(args, format);
 while (*ptr)
 {
-if (*ptr == '%' && *(ptr + 1) == '\0')
-return (-1);
 if (*ptr == '%' && *(ptr + 1))
 {
 ptr++;
@@ -33,6 +32,10 @@ str = "(null)";
 while (*str)
 count += write(1, str++, 1);
 }
+else if (*ptr == 'd' || *ptr == 'i')
+{
+count += print_integer(va_arg(args, int));
+}
 else if (*ptr == '%')
 {
 count += write(1, "%", 1);
@@ -40,9 +43,8 @@ count += write(1, "%", 1);
 else
 {
 count += write(1, ptr, 1);
-}
+}}
 ptr++;
-}
 va_end(args);
 return (count);
 }
